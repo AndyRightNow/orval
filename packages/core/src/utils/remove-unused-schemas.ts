db72caf42849d235d23ae23120bd24afbf098472
AndyRightNow <naruto199507@gmail.com>
2024-12-04 16:43:43 +0900

    feat: stub out removeUnusedSchemas

---
+++ packages/core/src/utils/remove-unused-schemas.ts
@@ -0,0 +1,8 @@
+import { OpenAPIObject, SchemasObject } from "openapi3-ts/oas30";
+
+export const removeUnusedSchemas = (
+  schemas: SchemasObject,
+  spec: OpenAPIObject,
+): SchemasObject => {
+  
+}
